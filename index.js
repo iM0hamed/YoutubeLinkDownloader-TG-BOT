94f2c4bde6396437220c8aebf8d9e6b7296b5335
ArmenMesropyan <armenbangits@gmail.com>
2021-01-06 01:23:20 +0400

    Fixed launch browser bug

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ bot.startWebhook(`/bot/${BOT_TOKEN}`, null, PORT);
 bot.launch();
 
 const findYoutubeDownloadLink = async (youtubeLink) => {
-    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
 
     await page.goto('https://ytmp3.cc/en13/');
